b3a06f2c99bdd2d16fd547a50f534c8fe9358a35
Lizzie Elmer <lizzieeelmer@hotmail.com>
2020-04-21 13:25:35 +0100

    21/04/20 Functional sample matching code
    Code that finds a unique matched non x-ray variable for each X-ray variable. Currently no size diff limit

--- sample_matching.py
+++ sample_matching.py
@@ -50,26 +50,8 @@ def get_differences(ob, noxsize, noxlum, noxz, abso=True):
         return sizediff, lumdiff, zdiff
     
 def find_matches(ob, lumrange, zrange, noxsize, noxlum, noxz):
-    print(ob['ID'])
-#    ### Extract arrays ###
-#    obsize = ob['FWHM_WORLD']*3600 # FWHM good estimate of size (converted to arcsecs)
-#    oblum = ob['M_K_z_p'] # Absolute mag = luminosity
-#    obz = ob['z_p'] # best photometric z so consistant with lum calcs
-#    
-##    print('FWHM = '+str(obsize))
-##    print('abso mag = '+str(oblum))
-##    print('z = '+str(obz))
-#
-#    ### Find differences ###
-#    sizediff = noxsize - obsize
-#    lumdiff = noxlum - oblum
-#    zdiff = noxz - obz
-#    
-#    ### Find absolute differences as we don't mind which is bigger ###
-#    abssizediff = np.abs(sizediff)
-#    abslumdiff = np.abs(lumdiff)
-#    abszdiff = np.abs(zdiff)
-    ### Find absolute differences as we don't mind which is bigger ###
+#    print(ob['ID'])
+    ### get differences ###
     abssizediff, abslumdiff, abszdiff = get_differences(ob, noxsize, noxlum, noxz)
     
     ### Sort by size match ###
@@ -105,12 +87,12 @@ def find_matches(ob, lumrange, zrange, noxsize, noxlum, noxz):
     ### append total number of matches ###
     num = len(goodinds)
     if num == 0:
-        print('No matches found')
+#        print('No matches found')
         ### save inds as nans to indicate no matches ###
         closeind = np.nan
         bestsizeind = np.nan
     else:
-        print(str(num)+' matches found')
+#        print(str(num)+' matches found')
     
         ### find closest overall match out of the good inds ###
         totdiff = abssizediff[goodinds] + abslumdiff[goodinds] + abszdiff[goodinds]
@@ -137,107 +119,114 @@ lumrange = 0.5
 zrange = 0.2
 
 #%% Iterate over objects ###
-allcloseinds = np.array([])
-allbestsizeinds = np.array([])
-matches = np.array([])
-for ob in xvarydata:
-    goodinds, closeind, bestsizeind = find_matches(ob, lumrange, zrange, 
-                                                   noxsize, noxlum, noxz)
-    allcloseinds = np.append(allcloseinds, closeind)
-    allbestsizeinds = np.append(allbestsizeinds, bestsizeind)
-    
-#    break # quit after one loop for when checking
-print(str(len(allbestsizeinds[np.isnan(allbestsizeinds)])) + ' with no matches')
-
-#%% Check each index to see if it is repeated ###
-allbestinds2 = np.zeros(len(xvarydata)) + 9999 # so 9999 indicates those that no longer have matches
-matchedinds = np.array([])
-for n, test in enumerate(allbestsizeinds):
-    if np.isnan(test) == True:
-        allbestinds2[n] = test # so stays as no matches
-        continue
-    elif np.isin(test, matchedinds) == True: #i.e. the no x object already has a match
-        continue # skip loop as already found best match for these objects
+allbestsizeinds = np.zeros(len(xvarydata)) + 9999 # so 9999 indicates those that do not have matches
+fulllen = len(allbestsizeinds)
+unique = np.unique(allbestsizeinds)
+uniquelen = len(unique)
+while uniquelen != fulllen:
+    for m, ob in enumerate(xvarydata):
+#        print(ob['ID'])
+        if allbestsizeinds[m] != 9999:
+            continue # skip if match already found
+        goodinds, closeind, bestsizeind = find_matches(ob, lumrange, zrange, 
+                                                       noxsize, noxlum, noxz)
+        ### test if the index has already been assigned to its best match ###
+        while np.isin(bestsizeind, unique) == True:
+#            print('index assigned')
+            ### This means the best match is taken so need to find next best ###
+            goodinds = goodinds[goodinds!=bestsizeind] # delete best option
+            testobs = noxvarydata[goodinds] # get nox data for other options
+            if len(testobs) == 0:
+#                print('No further matches available')
+                bestsizeind = np.nan # set to nan as no match
+                break # quit loop
+            else:
+                testnoxsize = noxsize[goodinds]
+                testnoxlum = noxlum[goodinds]
+                testnoxz = noxz[goodinds]
+                goodinds2, closeind2, bestsizeind2 = find_matches(ob, lumrange, zrange, 
+                                                               testnoxsize, 
+                                                               testnoxlum, 
+                                                               testnoxz)
+                ### apply mask to goodinds to get original inds not inds of minitable ###
+                goodinds = goodinds[goodinds2] 
+                closeind = goodinds[closeind2]
+                bestsizeind = goodinds[bestsizeind2]
         
-    test = test.astype(int) # as int so can use as index
-
-    ### get values for the nox matched object ###
-    testnoxsize = noxsize[test]
-    testnoxlum = noxlum[test]
-    testnoxz = noxz[test]
+        ### set value once loop quits ###
+        allbestsizeinds[m] = bestsizeind
+#        print('unique index found')
+        
+    #    break # quit after one loop for when checking
+    print(str(len(allbestsizeinds[np.isnan(allbestsizeinds)])) + ' with no matches')
+    unique = np.unique(allbestsizeinds)
+    uniquelen = len(unique)
+    print(str(uniquelen)+' unique indicies')
     
-    ### get the matched x ray ob data ###
-    mask = np.isin(allbestsizeinds, test)
-    testobs = xvarydata[mask]
-    
-    ### create index array for ease of locating ###
-    indarr = np.arange(len(xvarydata))
-    
-    ### Test which object is closest ###
-    if len(testobs) == 1:
-        allbestinds2[n] = test
-        continue # as this means there is only one object matched to it
-    else:
-        testsizediff = np.array([])
-        for ob in testobs:
-            abssizediff, abslumdiff, abszdiff = get_differences(ob, testnoxsize, 
-                                                                testnoxlum, testnoxz)
-            testsizediff = np.append(testsizediff, abssizediff)
+    #%% Check each index to see if it is repeated ###
+    allbestinds2 = np.zeros(len(xvarydata)) + 9999 # so 9999 indicates those that no longer have matches
+    matchedinds = np.array([])
+    for n, test in enumerate(allbestsizeinds):
+        if np.isnan(test) == True:
+            allbestinds2[n] = test # so stays as no matches
+            continue
+        elif np.isin(test, matchedinds) == True: #i.e. the no x object already has a match
+            continue # skip loop as already found best match for these objects
             
-        ### get minimum size difference and isolate its data ###
-        smalldiff = np.argmin(testsizediff)
-        bestob = testobs[smalldiff]
+        test = test.astype(int) # as int so can use as index
+    
+        ### get values for the nox matched object ###
+        testnoxsize = noxsize[test]
+        testnoxlum = noxlum[test]
+        testnoxz = noxz[test]
         
-        ### get original index in xvarydata ###
-        mask = np.isin(xvarydata['ID'], bestob['ID'])
+        ### get the matched x ray ob data ###
+        mask = np.isin(allbestsizeinds, test)
+        testobs = xvarydata[mask]
         
-        ### set value in allbestinds2 to test at that index ###
-        if allbestinds2[mask] == 9999:
-            allbestinds2[mask] = test
+        ### create index array for ease of locating ###
+        indarr = np.arange(len(xvarydata))
+        
+        ### Test which object is closest ###
+        if len(testobs) == 1:
+            allbestinds2[n] = test # as this means there is only one object matched to it
+#            ### save test in an array that indicates this has already been matched ###
+#            matchedinds = np.append(matchedinds, test)
+#            continue 
         else:
-            print('Error: object already matched')
-            break # quit so I can build in capability for this
+            testsizediff = np.array([])
+            for ob in testobs:
+                abssizediff, abslumdiff, abszdiff = get_differences(ob, testnoxsize, 
+                                                                    testnoxlum, testnoxz)
+                testsizediff = np.append(testsizediff, abssizediff)
+                
+            ### get minimum size difference and isolate its data ###
+            smalldiff = np.argmin(testsizediff)
+            bestob = testobs[smalldiff]
+            
+            ### get original index in xvarydata ###
+            mask = np.isin(xvarydata['ID'], bestob['ID'])
+            
+            ### set value in allbestinds2 to test at that index ###
+            if allbestinds2[mask] == 9999:
+                allbestinds2[mask] = test
+#                print('Repeat resolved')
+            else:
+                print('Error: object already matched')
+                break # quit so I can build in capability for this
+            
+        ### save test in an array that indicates this has already been matched ###
+        matchedinds = np.append(matchedinds, test)
         
-    ### save test in an array that indicates this has already been matched ###
-    matchedinds = np.append(matchedinds, test)
-        
-#%% Rerun matching to get matches for those that were previously repeats ###
+    print(str(len(allbestinds2[np.isnan(allbestinds2)])) + ' with no matches')
+
+    ### Set arrays and vals for start of next loop ###
+    allbestsizeinds = allbestinds2
+    unique = np.unique(allbestsizeinds)
+    uniquelen = len(unique)
+    print(str(uniquelen)+' unique indicies')
     
-allbestinds3 = np.array([])
-for n, ob in enumerate(xvarydata):
-    if allbestinds2[n] != 9999: #ie it has a match or has no matches
-        ### Set index and continue ###
-        allbestinds3 = np.append(allbestinds3, allbestinds2[n])
-        continue
-    goodinds, closeind, bestsizeind = find_matches(ob, lumrange, zrange, 
-                                                   noxsize, noxlum, noxz)
-    while np.isin(bestsizeind, matchedinds) == True:
-        ### This means the best match is taken so need to find next best ###
-        goodinds = goodinds[goodinds!=bestsizeind] # delete best option
-        testobs = noxvarydata[goodinds] # get nox data for other options
-        if len(testobs) == 0:
-            print('No further matches available')
-            allbestinds3 = np.append(allbestinds3, np.nan) # set to nan as no match
-            break # quit loop
-        else:
-            testnoxsize = noxsize[goodinds]
-            testnoxlum = noxlum[goodinds]
-            testnoxz = noxz[goodinds]
-            goodinds2, closeind2, bestsizeind2 = find_matches(ob, lumrange, zrange, 
-                                                           testnoxsize, 
-                                                           testnoxlum, 
-                                                           testnoxz)
-            ### apply mask to goodinds to get original inds not inds of minitable ###
-            goodinds = goodinds[goodinds2] 
-            closeind = goodinds[closeind2]
-            bestsizeind = goodinds[bestsizeind2]
     
-    if len(allbestinds3) != n+1: #to test if nan has been returned
-        ### set new ind value and append to matched inds ###
-        allbestinds3 = np.append(allbestinds3, bestsizeind)
-        matchedinds = np.append(matchedinds, bestsizeind)
-    
-print(str(len(allbestinds3[np.isnan(allbestinds3)])) + ' with no matches')
 end = time.time()
 print('time (s) = '+str(end-start))
     
